11c160d61def460fe408c53724833dcfbd0facee
MUCHIRIERIC <muchirimunene031@gmail.com>
2026-08-12 00:36:18 -0700

    Update next.config.ts

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -4,10 +4,7 @@ const nextConfig = {
     // Allows production builds to successfully complete even with type errors
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Allows production builds to complete even with ESLint errors
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
